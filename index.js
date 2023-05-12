eab0ff7acbbe4947a9e4a6778b5dd06671a89cf7
Yasudh <133296209+Yasudh@users.noreply.github.com>
2023-05-11 20:47:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yesbtn = document.querySelector('#yesbtn');
+yesbtn.addEventListener('click',function () {
+    alert('Ay chi miamor? UwU')
+});
+
+const clarobtn = document.querySelector('#clarobtn');
+clarobtn.addEventListener('click',function (){
+    alert('JIJIJI<3')
+})
